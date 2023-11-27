f1c10131fa55b0bfd179fff5d8568c56ee14f29b
dmario24 <becky2sawyer@gmail.com>
2023-11-27 14:15:03 +0900

    react-three-fiber getting-started/introduction
    - https://docs.pmnd.rs/react-three-fiber/getting-started/introduction

--- src/components/Rhythm.tsx
+++ src/components/Rhythm.tsx
@@ -1,12 +1,40 @@
 import imgRhythm from '/public/images/kws.jpeg';
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
+function Box(props) {
+    // This reference will give us direct access to the mesh
+    const meshRef = useRef()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+    // Return view, these are regular three.js elements expressed in JSX
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 1.5 : 1}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
 
 
 const Rhythm = () => {
     return (
-        <div id="rhythm">
-            <img src={imgRhythm} />
-        </div>
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+            </Canvas>
     );
 };
 
