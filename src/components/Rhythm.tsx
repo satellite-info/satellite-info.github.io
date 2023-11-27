4f7a49681ea730d168d35e63fdac92f66ad70851
dmario24 <becky2sawyer@gmail.com>
2023-11-27 12:01:27 +0900

    add rhythm component

---
+++ src/components/Rhythm.tsx
@@ -0,0 +1,13 @@
+import imgRhythm from '/public/images/kws.jpeg';
+
+
+
+const Rhythm = () => {
+    return (
+        <div id="rhythm">
+            <img src={imgRhythm} />
+        </div>
+    );
+};
+
+export default Rhythm;
