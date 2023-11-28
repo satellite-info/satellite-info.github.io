85924d903e83e580b47b2edb7a6edb420a81149d
dmario24 <becky2sawyer@gmail.com>
2023-11-28 17:13:24 +0900

    orbit animation

--- src/components/Oa.tsx
+++ src/components/Oa.tsx
@@ -1,17 +1,41 @@
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 const Oa = () => {
 
     return (
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[0, 0, 0]} />
-            <mesh>
-                <sphereGeometry args={[1, 32, 32]} />
-                <meshStandardMaterial color="#E1DC59" />
-            </mesh>
+        <Canvas camera={{ position: [0, 20, 25], fov: 45 }}>
+            <Sun />
+            <Planet />
+            <Lights />
+            <OrbitControls />
         </Canvas>
     );
 };
+
+function Sun() {
+    return (
+        <mesh>
+            <sphereGeometry args={[2.5, 32, 32]} />
+            <meshStandardMaterial color="#E1DC59" />
+        </mesh>
+    );
+}
+function Planet() {
+    return (
+        <mesh position={[8, 0, 0]}>
+            <sphereGeometry args={[1, 32, 32]} />
+            <meshStandardMaterial color="#78D481" />
+        </mesh>
+    );
+}
+function Lights() {
+    return (
+        <>
+            <ambientLight />
+            <pointLight position={[0, 0, 0]} />
+        </>
+    );
+}
 
 export default Oa;
