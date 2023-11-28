19300653ccbb4ef0de33271526fa4863215389a1
dmario24 <becky2sawyer@gmail.com>
2023-11-28 17:08:35 +0900

    add lights

--- src/components/Oa.tsx
+++ src/components/Oa.tsx
@@ -4,6 +4,8 @@ const Oa = () => {
 
     return (
         <Canvas>
+            <ambientLight />
+            <pointLight position={[0, 0, 0]} />
             <mesh>
                 <sphereGeometry args={[1, 32, 32]} />
                 <meshStandardMaterial color="#E1DC59" />
