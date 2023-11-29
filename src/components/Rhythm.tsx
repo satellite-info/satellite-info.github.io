58e1e279d176ac4a1c099d8704f06b03ab4c92a4
dmario24 <becky2sawyer@gmail.com>
2023-11-29 17:44:03 +0900

    touch img size up

--- src/components/Rhythm.tsx
+++ src/components/Rhythm.tsx
@@ -1,36 +1,44 @@
 import { BlurFilter } from 'pixi.js';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
+import { Stage, Container, Sprite } from '@pixi/react';
 import { useMemo, useState, useEffect } from 'react';
 
 const Rhythm = () => {
     const blurFilter = useMemo(() => new BlurFilter(4), []);
     const [satelliteRotation, setSatelliteRotation] = useState(0);
+    const [satelliteScale, setSatelliteScale] = useState(1);
 
     useEffect(() => {
-        // 일정 간격으로 회전 각도 업데이트
         const intervalId = setInterval(() => {
-            // 회전 각도를 일정량씩 증가 (예시: 0.1 라디안)
-            setSatelliteRotation((prevRotation) => prevRotation + 0.01);
+            setSatelliteRotation((prevRotation) => prevRotation + 0.1);
         }, 100);
 
-        // 컴포넌트가 언마운트되면 interval 정리
         return () => clearInterval(intervalId);
-    }, []); // 빈 배열을 전달하여 최초 렌더링 시에만 실행
+    }, []);
+
+    const handleTouch = () => {
+        setSatelliteScale((prevScale) => prevScale * 2);
+    };
+
+    const handleRelease = () => {
+        setSatelliteScale(1); // 손가락을 뗄 때 기존 크기로 되돌리기
+    };
 
     const viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     const viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
     return (
         <Stage width={viewportWidth} height={viewportHeight} options={{ resizeTo: window }}>
-            <Sprite
-                image="../../public/images/satellite.png"
-                width={200}
-                height={200}
-                x={viewportWidth/2}
-                y={viewportHeight/2}
-                anchor={{ x: 0.5, y: 0.5 }}
-                rotation={satelliteRotation}
-            />
+            <Container interactive={true} pointerdown={handleTouch} pointerup={handleRelease}>
+                <Sprite
+                    image="../../public/images/satellite.png"
+                    width={50 * satelliteScale}
+                    height={50 * satelliteScale}
+                    x={viewportWidth / 2}
+                    y={viewportHeight / 2}
+                    anchor={{ x: 0.5, y: 0.5 }}
+                    rotation={satelliteRotation}
+                />
+            </Container>
         </Stage>
     );
 };
